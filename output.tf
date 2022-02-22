8d69b633041c3c9edb2236641a357cbedf3a0326
Daniel Sheldon <daniel.sheldon@suse.com>
2022-02-22 09:44:16 +0000

    correcting values

--- output.tf
+++ output.tf
@@ -1,25 +1,25 @@
 # Outputs
 
 output "kubeconfig_api_server_url" {
-  value = rke_cluster.rancher_cluster.api_server_url
+  value = rke_cluster.cluster.api_server_url
 }
 
 output "kubeconfig_client_cert" {
-  value = rke_cluster.rancher_cluster.client_cert
+  value = rke_cluster.cluster.client_cert
   sensitive = true
 }
 
 output "kubeconfig_client_key" {
-  value = rke_cluster.rancher_cluster.client_key
+  value = rke_cluster.cluster.client_key
   sensitive = true
 }
 
 output "kubeconfig_ca_crt" {
-  value = rke_cluster.rancher_cluster.ca_crt
+  value = rke_cluster.cluster.ca_crt
   sensitive = true
 }
 
 output "kubeconfig_yaml" {
-  value = rke_cluster.rancher_cluster.kube_config_yaml
+  value = rke_cluster.cluster.kube_config_yaml
   sensitive = true
 }
